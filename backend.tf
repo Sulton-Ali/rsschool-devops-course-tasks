e274e97aab22a33e1b73665e78b335322295aa54
Sultonali Jalolov <jssalijalol@gmail.com>
2025-06-15 14:02:37 +0500

    refactor: format terraform file

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "rsschool-terraform-state-storage"
-    key    = "tasks/terraform.tfstate"
-    region = "ap-northeast-1"
+    bucket  = "rsschool-terraform-state-storage"
+    key     = "tasks/terraform.tfstate"
+    region  = "ap-northeast-1"
     encrypt = true
   }
 }
