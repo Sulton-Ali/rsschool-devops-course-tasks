c8abbeb5067c34939a6642f2c4ec376317b984fc
Sultonali Jalolov <jssalijalol@gmail.com>
2025-06-15 14:27:38 +0500

    refactor: update fingerprint

--- iam-oidc.tf
+++ iam-oidc.tf
@@ -6,7 +6,7 @@ resource "aws_iam_openid_connect_provider" "github" {
   ]
 
   thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "2b18947a6a9fc7764fd8b5fb18a863b0c6dac24f"
   ]
 
 }
