81f8ad2120105ef145c7add44bff36451e3f437c
Sultonali Jalolov <jssalijalol@gmail.com>
2025-06-22 11:32:41 +0500

    fix: code error

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,6 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name        = "MyDevOpsVPC"
-    Environment = "dev"
+    Name = "MyDevOpsVPC"
   }
 }
